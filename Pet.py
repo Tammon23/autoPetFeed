4ec79f0d2525ef2ab60ac8bbb869fc808794c6be
Tammon23 <Tammon2000@gmail.com>
2021-01-13 01:52:39 -0500

    Added Pet class which contains information about a pet

---
+++ Pet.py
@@ -0,0 +1,7 @@
+class Pet:
+    def __init__(self, exercise, drink, food, name, slot_num=-1, page=1):
+        self.exercise = exercise
+        self.drink = drink
+        self.food = food
+        self.name = name
+        self.slot_num = slot_num
